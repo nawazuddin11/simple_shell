b522d0361f87930005ca127068874cbbc759ddd8
Ahmad Suleiman Bello <120317441+Ai-Maven@users.noreply.github.com>
2023-11-16 00:19:57 +0100

    latest shell.h commit

--- shell.h
+++ shell.h
@@ -13,18 +13,18 @@
 #include <fcntl.h>
 #include <errno.h>
 
-/* for i/o buffers */
+/* for i/o buffer */
 #define READ_BUF_SIZE 1024
 #define WRITE_BUF_SIZE 1024
 #define BUF_FLUSH -1
 
-/* for piping  */
+/* for pipingg  */
 #define CMD_NORM	0
 #define CMD_OR		1
 #define CMD_AND		2
 #define CMD_CHAIN	3
 
-/* for convert_number() */
+/* forr convert_number() */
 #define CONVERT_LOWERCASE	1
 #define CONVERT_UNSIGNED	2
 

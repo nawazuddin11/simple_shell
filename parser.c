799b3f5f150f3bb3764d1915d5b2360c4f548296
Ahmad Suleiman Bello <120317441+Ai-Maven@users.noreply.github.com>
2023-11-16 00:18:22 +0100

    latest parser.c commit

--- parser.c
+++ parser.c
@@ -1,10 +1,10 @@
 #include "shell.h"
 /**
- * is_cmd - determines if a file is an executable command
+ * is_cmd - determine if a file is an executable command
  * @info: the info struct
  * @path: path to the file
  *
- * Return: 1 if true, 0 otherwise
+ * Return: 1 if true, 0 other
  */
 int is_cmd(info_t *info, char *path)
 {

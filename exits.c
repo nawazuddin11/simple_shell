9f6f8201b5c4e8cee34c734a4068921d9a291178
Ahmad Suleiman Bello <120317441+Ai-Maven@users.noreply.github.com>
2023-11-16 00:11:43 +0100

    exit latest commit

--- exits.c
+++ exits.c
@@ -1,10 +1,10 @@
 #include "shell.h"
 /**
- **_strncpy - copies a string
+ **_strncpy - copie a string
  *@dest: the destination string to be copied to
  *@src: the source string
- *@n: the amount of characters to be copied
- *Return: the concatenated string
+ *@n: the amoun of characters to be copied
+ *Return: the concatenate string
  */
 char *_strncpy(char *dest, char *src, int n)
 {

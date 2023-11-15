e81c8b158231a94df5c97338eae7490e08017532
Ahmad Suleiman Bello <120317441+Ai-Maven@users.noreply.github.com>
2023-11-16 00:11:09 +0100

    latest commit

--- errors1.c
+++ errors1.c
@@ -1,8 +1,8 @@
 #include "shell.h"
 /**
- * _erratoi - converts a string to an integer
+ * _erratoi - convert a string to an integer
  * @s: the string to be converted
- * Return: 0 if no numbers in string, converted number otherwise
+ * Return: 0 if no numbers in string, conver number otherwise
  *       -1 on error
  */
 int _erratoi(char *s)

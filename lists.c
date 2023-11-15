762b9e647b868d6426c3d9c0ec040f10e2ddaced
Ahmad Suleiman Bello <120317441+Ai-Maven@users.noreply.github.com>
2023-11-16 00:15:35 +0100

    latestl lists commit

--- lists.c
+++ lists.c
@@ -1,9 +1,9 @@
 #include "shell.h"
 /**
- * add_node - adds a node to the start of the list
+ * add_node - add a node to the start of the list
  * @head: address of pointer to head node
- * @str: str field of node
- * @num: node index used by history
+ * @str: str fields of node
+ * @num: nodes index used by history
  *
  * Return: size of list
  */
